5fbd6cbf33d3c237b319bccf5b3fe8fa9648f07d
Sungjun Moon <project_m_@hotmail.com>
2020-09-25 21:38:20 +0900

    initial commit

---
+++ bidetworldwebserver/src/main/java/com/ddnsfree/bidetworld/bidetworldwebserver/bidetworldwebserver/BidetworldwebserverApplication.java
@@ -0,0 +1,13 @@
+package com.ddnsfree.bidetworld.bidetworldwebserver.bidetworldwebserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BidetworldwebserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BidetworldwebserverApplication.class, args);
+	}
+
+}
---
+++ bidetworldwebserver/src/test/java/com/ddnsfree/bidetworld/bidetworldwebserver/bidetworldwebserver/BidetworldwebserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddnsfree.bidetworld.bidetworldwebserver.bidetworldwebserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BidetworldwebserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
